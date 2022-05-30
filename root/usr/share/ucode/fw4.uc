fullcone-nat-nftables/openwrt-firewall4-with-fullcone

commit 57984e0369974e3cf5830db4dfe5e93e8c494b09

fw4: always resolve lower flowtable devices
Do not restrict lower device resolving to hardware flow offloading, it is
required for software flow offloading as well.

While we're at it, also refactor and simplify the code to not require ubus
runtime state for device resolving anymore.

Fixes: #9854
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

root/usr/share/ucode/fw4.uc
@@ -356,26 +356,53 @@ function map_setmatch(set, match, proto) {
 	return fields;
 }
 
-function resolve_lower_devices(devstatus, devname) {
-	let dir = fs.opendir(`/sys/class/net/${devname}`);
-	let devs = [];
+function determine_device_type(devname) {
+	let uevent = fs.open(`/sys/class/net/${devname}/uevent`),
+	    devtype = null;
 
-	if (dir) {
-		if (!devstatus || devstatus[devname]?.["hw-tc-offload"]) {
-			push(devs, devname);
+	if (uevent) {
+		let line;
+
+		while ((line = uevent.read('line')) != null) {
+			let m = match(line, /^DEVTYPE=(\w+)/);
+
+			if (m) {
+				devtype = m[1];
+				break;
+			}
 		}
-		else {
+
+		uevent.close();
+	}
+
+	return devtype;
+}
+
+function resolve_lower_devices(devname) {
+	switch (determine_device_type(devname)) {
+	case null:
+		return [];
+
+	case 'vlan':
+	case 'bridge':
+		let dir = fs.opendir(`/sys/class/net/${devname}`);
+		let lower = [];
+
+		if (dir) {
 			let e;
 
 			while ((e = dir.read()) != null)
 				if (index(e, "lower_") === 0)
-					push(devs, ...resolve_lower_devices(devstatus, substr(e, 6)));
+					push(lower, ...resolve_lower_devices(substr(e, 6)));
+
+			dir.close();
 		}
 
-		dir.close();
-	}
+		return lower;
 
-	return devs;
+	default:
+		return [ devname ];
+	}
 }
 
 function nft_json_command(...args) {
@@ -433,40 +460,24 @@ return {
 	},
 
 	resolve_offload_devices: function() {
-		if (!this.default_option("flow_offloading"))
-			return [];
-
-		let devstatus = null;
 		let devices = [];
 
-		if (this.default_option("flow_offloading_hw")) {
-			let bus = ubus.connect();
-
-			if (bus) {
-				devstatus = bus.call("network.device", "status") || {};
-				bus.disconnect();
-			}
-
+		if (this.default_option("flow_offloading")) {
 			for (let zone in this.zones())
 				for (let device in zone.related_physdevs)
-					push(devices, ...resolve_lower_devices(devstatus, device));
+					push(devices, ...resolve_lower_devices(device));
 
-			devices = uniq(devices);
+			if (length(devices)) {
+				devices = sort(uniq(devices));
 
-			if (nft_try_hw_offload(devices))
-				return devices;
-
-			this.warn('Hardware flow offloading unavailable, falling back to software offloading');
-			this.state.defaults.flow_offloading_hw = false;
-
-			devices = [];
+				if (this.default_option("flow_offloading_hw") && !nft_try_hw_offload(devices)) {
+					this.warn('Hardware flow offloading unavailable, falling back to software offloading');
+					this.state.defaults.flow_offloading_hw = false;
+				}
+			}
 		}
 
-		for (let zone in this.zones())
-			for (let device in zone.match_devices)
-				push(devices, ...resolve_lower_devices(null, device));
-
-		return uniq(devices);
+		return devices;
 	},
 
 	check_set_types: function() {

tests/01_configuration/01_ruleset
@@ -296,10 +296,10 @@ table inet fw4 {
 [!] Section @defaults[0] specifies unknown option 'unknown_defaults_option'
 [!] Section @rule[9] (Test-Deprecated-Rule-Option) option '_name' is deprecated by fw4
 [!] Section @rule[9] (Test-Deprecated-Rule-Option) specifies unknown option 'unknown_rule_option'
-[call] ctx.call object <network.device> method <status> args <null>
+[call] fs.open path </sys/class/net/br-lan/uevent> mode <null>
 [call] fs.opendir path </sys/class/net/br-lan>
-[call] fs.opendir path </sys/class/net/eth0>
-[call] fs.opendir path </sys/class/net/eth1>
+[call] fs.open path </sys/class/net/eth0/uevent> mode <null>
+[call] fs.open path </sys/class/net/eth1/uevent> mode <null>
 [call] system command </usr/sbin/nft -c '
 		add table inet fw4-hw-offload-test;
 		add flowtable inet fw4-hw-offload-test ft {

commit 6dd26176b684de6e16296d78660f35fe5c150e8d

fw4: fix logic flaw in testing hw flow offloading support
The revised logic failed to account for cases where zero offload capable
devices where resolved, in this case the capability check was not performed
at all.

Fixes: #9935
Fixes: 57984e0 ("fw4: always resolve lower flowtable devices")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

root/usr/share/ucode/fw4.uc
@@ -462,17 +462,15 @@ return {
 				for (let device in zone.related_physdevs)
 					push(devices, ...resolve_lower_devices(devstatus, device, true));
 
-			if (length(devices)) {
-				devices = sort(uniq(devices));
+			devices = sort(uniq(devices));
 
-				if (nft_try_hw_offload(devices))
-					return devices;
+			if (length(devices) && nft_try_hw_offload(devices))
+				return devices;
 
-				this.warn('Hardware flow offloading unavailable, falling back to software offloading');
-				this.state.defaults.flow_offloading_hw = false;
+			this.warn('Hardware flow offloading unavailable, falling back to software offloading');
+			this.state.defaults.flow_offloading_hw = false;
 
-				devices = [];
-			}
+			devices = [];
 		}
 
 		for (let zone in this.zones())